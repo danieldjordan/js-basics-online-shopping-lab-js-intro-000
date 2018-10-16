0b8b1b94eca08876e20fa932fa48737089aa4fed
danieldjordan <danieldjordan@gmail.com>
2018-10-16 15:11:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var cart = [];
 
-function getCart() {
+function getCart(item) {
+
  return cart;
 }
 
@@ -10,21 +11,33 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  let price = (Math.floor(Math.random() * 100))
+  var itemVar = {itemName: item, itemPrice: price};
+  console.log(itemVar)
+  cart.push(itemVar)
+  return `${itemVar.itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
-}
-
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
+  if(cart.length > 0){
+    var message = 'In your cart, '
+    for(var i = 0; i<cart.length; i++){
+      if(i === 0){
+        message += `you have ${cart[i].itemName} at $${cart[i].itemPrice}`
+      }
+      else if(i == (cart.length-1)&&(cart.length>1)){
+        message += `, and ${cart[i].itemName} at $${cart[i].itemPrice}`
+      }
+      else{
+        message += `, ${cart[i].itemName} at $${cart[i].itemPrice}`
+      }
+    }
+    message += '.'
+  }
+  else{
+    message = 'Your shopping cart is empty.'
+  }
+  return message
+ }
+ 
+ 
